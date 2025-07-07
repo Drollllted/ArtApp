b7bb91ef261ef80530a840e0a5c9568f5f34a176
Artem Tarasov <artemdrolllted@gmail.com>
2025-07-07 07:20:41 +0300

    Initial Commit

---
+++ ArtApp/ArtAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAppApp.swift
+//  ArtApp
+//
+//  Created by Drolllted on 07.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArtAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
