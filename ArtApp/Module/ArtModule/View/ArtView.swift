60c7a4fec42c5e4f724391b66f045c11ff462270
Artem Tarasov <artemdrolllted@gmail.com>
2025-07-10 01:58:48 +0300

    Step 2 - setup ArtView

--- ArtApp/Module/ArtModule/View/ArtView.swift
+++ ArtApp/Module/ArtModule/View/ArtView.swift
@@ -5,4 +5,20 @@
 //  Created by Drolllted on 07.07.2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct ArtView: View {
+    var body: some View {
+        NavigationStack{
+            VStack(alignment: .leading, spacing: 20) {
+                Image(systemName: "photo")
+            }
+            .navigationTitle("Title")
+            .toolbarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    ArtView()
+}
